f077462445c39eef454e913c3dc7bb230328ccac
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2021-10-18 18:03:31 +0200

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import QrScanner from 'qr-scanner';
 
 function App() {
+
+
+  const onClick = () => {
+    const videoElement = document.querySelector('video');
+    console.log(videoElement)
+    if(videoElement != null){
+      const qrScanner = new QrScanner(videoElement, result => console.log('decoded qr code:', result));
+      console.log(qrScanner)
+      qrScanner.start();
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <video id="qr-video"></video>
+        <button onClick={onClick}>toto</button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
